33f173768b99e6b88f5c85e9752d744e4341ef86
Wilkson Albuquerque Carvalho <wilkson.carvalho@navbrasil.gov.br>
2026-07-21 14:31:09 -0300

    Update base path configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Permite que os caminhos dos scripts funcionem em subpastas como o GitHub Pages
+  base: '/miniboxmilena/', // Caminho correto para o repositório no GitHub Pages
 })
